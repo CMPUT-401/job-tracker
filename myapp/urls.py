6044c2e4c0d74034d61dc52f4363774d98b9f1b9
Janan Panchal <jpanchal@ualberta.ca>
2025-01-17 21:16:22 -0700

    Add Resume tailor button

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views  # Import your views from the app
 
 urlpatterns = [
     path('', views.home, name='home'),  # Root URL ('/') shows the home view
+    path('resume-tailor/', views.resume_tailor, name='resume_tailor'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def resume_tailor(request):
+    # Simulating a master resume for now
+    master_resume = [
+        "Developed REST APIs using Django and Flask.",
+        "Optimized database queries, improving performance by 25%.",
+        "Led a team of 5 to deliver high-quality software projects.",
+        "Proficient in Python, JavaScript, and SQL.",
+        "Collaborated with cross-functional teams to achieve project goals."
+    ]
+    blank_resume = []
+
+    if request.method == 'POST':
+        # Logic to handle copying items from master to blank resume
+        selected_items = request.POST.getlist('selected_items')
+        blank_resume.extend(selected_items)
+
+    return render(request, 'tracker/resume_tailor.html', {
+        'master_resume': master_resume,
+        'blank_resume': blank_resume,
+    })
